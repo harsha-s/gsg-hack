f467599f84a6e3a68a323dde687a8871685d93b2
Vinh-Tuong <vinhtuong.mai@joingsg.com>
2024-02-22 07:29:29 +0100

    first step

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,107 @@
 import Link from "next/link";
+import 'normalize.css/normalize.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import styles from '../styles/index.module.css';
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [number, setNumber] = useState(30);
+  const [username, setUsername] = useState(null);
+  const [formUsername, setFormUsername] = useState(null);
+  const [merchant, setMerchant] = useState('');
+  const [merchants, setMerchants] = useState([]);
+  const [newsletter, setNewsletter] = useState(false);
+
+  const onMerchantAdd = (name) => {
+    if (merchants.includes(name)) {
+      return;
+    }
+
+    setMerchants([...merchants, name]);
+    setMerchant('');
+  };
+
+  const onMerchantRemove = (name) => {
+    setMerchants(merchants.filter(mer => mer !== name))
+  };
+
+  const handleLoginBtnClick = () => {
+    if (!formUsername) {
+      return;
+    }
+
+    localStorage.setItem('username', formUsername);
+    setUsername(formUsername);
+  };
+
+  const handleLogoutBtnClick = () => {
+    localStorage.removeItem('username');
+    setUsername(null);
+  };
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
+
+  if (!username) {
+    return <main>
+      <input type="text" value={formUsername} onChange={(event) => setFormUsername(event.target.value)} placeholder="Username"/>
+      <input type="text" value="" placeholder="*******"/>
+      <button onClick={handleLoginBtnClick}>Login</button>
+    </main>
+  }
+
   return (
-    <ul>
-      <li>
-        <Link href="/a" as="/a">
-          a
-        </Link>
-      </li>
-      <li>
-        <Link href="/b" as="/b">
-          b
-        </Link>
-      </li>
-    </ul>
+    <main className={styles.main}>
+      <nav>
+        Hello {username},
+        <button onClick={handleLogoutBtnClick}>Logout</button>
+      </nav>
+
+      <section>
+        <h1>Earn $5 bonus from us</h1>
+        <h3>Subscribe to our Newsletter <span className={`${styles.status} ${newsletter ? styles.done : styles.notyet}`}>{newsletter ? 'Done' : 'Not yet'}</span></h3>
+        <input id="newsletter" type="checkbox" value={newsletter ? 'checked' : ''} onChange={(event) => setNewsletter(event.target.checked)} />
+        <label htmlFor="newsletter">Daily newsletter</label>
+        <h3>Add your favorite merchants <span className={`${styles.status} ${merchants.length > 0 ? styles.done : styles.notyet}`}>{merchants.length > 0 ? 'Done' : 'Not yet'}</span></h3>
+        <input type="text" value={merchant} onKeyDown={(e) => { if (e.key === "Enter") { onMerchantAdd(merchant) }
+        }} onChange={(event) => setMerchant(event.target.value)} placeholder="Your favorite merchant" />
+        <ul>
+          {merchants.map(name =>
+            <div className={styles.favoriateMerchant}>
+              <span>{name}</span>
+              {' '}
+              <button onClick={() => onMerchantRemove(name)}>x</button>
+            </div>
+          )}
+        </ul>
+        {newsletter && merchants.length > 0 && <button>Claim your $5 bonus</button>}
+      </section>
+      
+      <hr />
+
+      <section>
+        <h1>Get your cashback</h1>
+        <h3>Your purchase value</h3>
+        <input type="number" max={100} onChange={(event) => setNumber(+event.target.value)} value={number} />
+        <br />
+        <p></p>
+        <div className={styles.progressBar}>
+          <div className={styles.progressBarInner} style={{ width: `${number}%` }}></div>
+          <div className={`${styles.firstItem} ${number >= 32 ? styles.active : ''}`} style={{ left: '30%' }}>
+          First
+          </div>
+          <div className={`${styles.secondItem} ${number >= 62 ? styles.active : ''}`} style={{ left: '60%' }}>
+          Second
+          </div>
+          <div className={`${styles.lastItem} ${number >= 92 ? styles.active : ''}`} style={{ left: '90%' }}>
+          Last
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
